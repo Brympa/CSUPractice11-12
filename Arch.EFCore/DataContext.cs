793193a2a141755a7734544020b657f975fbb205
Vadim Rudi <mrbrempy@gmail.com>
2026-06-02 15:42:00 +0500

    Добавил User в DataContext

--- Arch.EFCore/DataContext.cs
+++ Arch.EFCore/DataContext.cs
@@ -14,4 +14,5 @@ public class DataContext : DbContext
     
     public DbSet<Student> Students => Set<Student>();
     public DbSet<Note> Notes => Set<Note>();
+    public DbSet<User> Users => Set<User>();
 }
